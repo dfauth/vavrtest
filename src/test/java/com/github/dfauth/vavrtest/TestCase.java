f12ff538d411b946cf598b33e109ed9da3206476
Damian Fauth <dfauth@gmail.com>
2019-06-07 08:43:58 +0800

    pattern matching, extractors

--- src/test/java/com/github/dfauth/vavrtest/TestCase.java
+++ src/test/java/com/github/dfauth/vavrtest/TestCase.java
@@ -6,10 +6,16 @@ import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
+import io.vavr.match.annotation.Patterns;
+import io.vavr.match.annotation.Unapply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import static com.github.dfauth.vavrtest.TestCase_EmployeePatterns.$employee;
+import static io.vavr.API.*;
+import static io.vavr.Patterns.$Failure;
+import static io.vavr.Patterns.$Success;
 import static org.testng.Assert.*;
 
 public class TestCase {
@@ -78,4 +84,65 @@ public class TestCase {
         logger.info("lazy.isEvaluated(): "+lazy.isEvaluated()+" val2: "+val2);
         assertEquals(val1, val2, 0);
     }
+
+    @Test
+    public void testMatch() {
+        int input = 2;
+        String output = Match(input).of(
+                Case($(1), "one"),
+                Case($(2), "two"),
+                Case($(3), "three"),
+                Case($(), "?"));
+
+        assertEquals("two", output);
+    }
+
+    @Test
+    public void testMatchnTry() {
+
+        Try<Integer> result = Try.of(() -> 1 / 0);
+
+        int result1 = Match(result).of(
+                Case($Success($()), v -> {
+                    logger.info("match'n'try success: " + v);
+                    return v;
+                }),
+                Case($Failure($()), v -> {
+                    logger.info("match'n'try success: " + v);
+                    return 0;
+                })
+        );
+        assertEquals(result1, 0);
+    }
+
+    @Test
+    public void testUnapply() {
+        Employee person = new Employee("Carl", 1);
+
+        String result = Match(person).of(
+                Case($employee($("Carl"), $()),
+                        (name, id) -> "Carl has employee id "+id),
+                Case($(),
+                        () -> "not found"));
+
+        assertEquals("Carl has employee id 1", result);
+    }
+
+    @Patterns
+    static class Employee {
+
+        private final String name;
+        private final int id;
+
+        @Unapply
+        public static Tuple2<String, Integer> employee(Employee employee) {
+            return new Tuple2<>(employee.name, employee.id);
+        }
+
+        public Employee(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+    }
+
 }
